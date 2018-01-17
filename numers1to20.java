633886741d64d8cef1a06d4fa1a34f3e2e33e999
AsenGeorgiev <35175468+AsenGeorgiev@users.noreply.github.com>
2018-01-17 11:37:13 +0200

    03. Nums 1...20

---
+++ numers1to20.java
@@ -0,0 +1,13 @@
+public class numers1to20 {
+    public static void main(String args[])
+    {
+        int x = 1;
+
+        while (x <= 20)
+        {
+            System.out.println("Value of x:" + x);
+
+            x++;
+        }
+    }
+}
